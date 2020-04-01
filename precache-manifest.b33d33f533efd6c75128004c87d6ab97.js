cdc3bd7cc7533cf3e073e44cb5546fae610d6d40
Oswin Jerome <oswinjeromej@gmail.com>
2020-04-01 13:12:23 +0530

    Updates

---
+++ precache-manifest.b33d33f533efd6c75128004c87d6ab97.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f7d4d811941a307b7e1bb0ceeec993",
+    "url": "/nk-entertainers/index.html"
+  },
+  {
+    "revision": "509fe9a4ce948ff6a7c4",
+    "url": "/nk-entertainers/static/css/main.834f5f03.chunk.css"
+  },
+  {
+    "revision": "e6cee3e1c9c576a8a5c9",
+    "url": "/nk-entertainers/static/js/2.ab7c2310.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nk-entertainers/static/js/2.ab7c2310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509fe9a4ce948ff6a7c4",
+    "url": "/nk-entertainers/static/js/main.75fcf7d6.chunk.js"
+  },
+  {
+    "revision": "e62d6d09e245b4c62270",
+    "url": "/nk-entertainers/static/js/runtime-main.d1cc9fd0.js"
+  },
+  {
+    "revision": "a618539abfc3cf13390eab2903661f4e",
+    "url": "/nk-entertainers/static/media/bg.a618539a.jpeg"
+  },
+  {
+    "revision": "8f6488df4ed9cd5dfffae22f095e2279",
+    "url": "/nk-entertainers/static/media/img1.8f6488df.jpg"
+  },
+  {
+    "revision": "cb9e9ea15366d0b740e39023346afd21",
+    "url": "/nk-entertainers/static/media/img3.cb9e9ea1.jpg"
+  },
+  {
+    "revision": "f8dd2cd0de51598133ecd08b89a5d2fc",
+    "url": "/nk-entertainers/static/media/img4.f8dd2cd0.jpg"
+  }
+]);
